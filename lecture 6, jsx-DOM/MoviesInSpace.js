60409c74efd321fdc5f0800213d370637c9a5538
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-02-04 09:22:34 +0100

    Toggle In Space

--- lecture 6, jsx-DOM/MoviesInSpace.js
+++ lecture 6, jsx-DOM/MoviesInSpace.js
@@ -1,15 +1,24 @@
 
 
 let btn = document.querySelector('#MakeInSpace');
-debugger
 btn.addEventListener('click', () => {
-debugger
+
+let titel = document.querySelector('a').textContent;
+let titel2 = document.querySelectorAll('a');
+
+
+let res = titel.includes('In Space')
+
+
     if (document.querySelector('#MakeInSpace').checked==true) {
         document.querySelectorAll('a').forEach(x=>x.textContent += ' In Space')
 
-    } else {
-        document.querySelectorAll('a').forEach(x=>x.textContent -= ' In Space')
+    }
 
+    else if(res) {
+        for (const e of titel2) {
+            e.innerText = e.textContent.slice(0, -9)
+        }
     }
    
 })

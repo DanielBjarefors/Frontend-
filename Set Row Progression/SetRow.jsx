930def2782c44e6f9c8ac5f43ec2a80954a6ba08
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-02-06 15:50:22 +0100

    folders

---
+++ Set Row Progression/SetRow.jsx
@@ -0,0 +1,19 @@
+ // get td with "two" (first row, second column)
+//  let td = #table1.rows[0].cells[1];
+//  let td = tabl.rows[0].cells[1];
+//  let td = table.rows[0].cells[1];
+//  td.style.backgroundColor = "red"; // highlight it
+
+// let cells = document.querySelector("#table1").querySelector("td");
+// cells[0].
+
+// var x = document.querySelector("#table1").querySelectorAll("td");
+//     x[0].style.backgroundColor = "yellow";   
+
+    var x = document.querySelectorAll("tr");
+    x[7].style.backgroundColor = "yellow";   
+
+
+
+
+    

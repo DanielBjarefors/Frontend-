31a3fc00029b62e2a7264417832d82a8379cfe9b
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-02-07 11:32:24 +0100

    starrted tutorial

---
+++ YoutubeTutorialJavascript/Books.jsx
@@ -0,0 +1,16 @@
+let wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
+
+
+var books
+
+
+
+
+
+
+ // let titles = document.getElementsByClassName('title');
+
+// Array.from(item).forEach(function(item) {
+//     console.log(item);
+// });
